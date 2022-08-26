ae3dbeeeabf604e28a342a6ae8a3177bd3f3551a
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-08-26 22:49:12 +0100

    #7 WIP: working on adding Chakra

--- pages/signup.js
+++ pages/signup.js
@@ -1,26 +1,36 @@
-import { Container, Heading, Input } from "@chakra-ui/react";
+import { Box, Heading, HStack, Input, Stack } from "@chakra-ui/react";
 
 export default function Signup() {
   return (
-    <Container maxW="container.lg" my="6">
-      <Heading as="h1" size="xl">
-        Create a new account.
-      </Heading>
-      <Heading as="h2" size="lg">
-        Email
-      </Heading>
-      <Input type="email" placeholder="Enter your email address" />
-      <Heading as="h2" size="lg">
-        Password
-      </Heading>
-      <input type="password" placeholder="Enter your password" />
-      <button>SIGN UP</button>
+    <Stack
+      maxW="container.lg"
+      my="6"
+      boxShadow="base"
+      rounded="lg"
+      w={{ md: 1000, sm: "100%" }}
+      m="auto"
+    >
+      <Box>
+        <Heading as="h1" size="xl">
+          Create a new account.
+        </Heading>
+        <Heading as="h2" size="lg">
+          Email
+        </Heading>
+        <Input type="email" placeholder="Enter your email address" />
+        <Heading as="h2" size="lg">
+          Password
+        </Heading>
+        <input type="password" placeholder="Enter your password" />
+        <button>SIGN UP</button>
+        <p>OR</p>
+        <button>Continue with Google</button>
+      </Box>
 
-      <p>OR</p>
-      <button>Continue with Google</button>
-
-      <p>Already have an account?</p>
-      <a href="login">LOGIN</a>
-    </Container>
+      <Box>
+        <p>Already have an account?</p>
+        <a href="login">LOGIN</a>
+      </Box>
+    </Stack>
   );
 }
