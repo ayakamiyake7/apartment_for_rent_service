039e98b3b929cfd71c9f33bcfa5800278dd3daf3
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-09-09 22:24:42 +0100

    WIP: working on adding design

--- pages/create.js
+++ pages/create.js
@@ -2,6 +2,15 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { useRecoilState } from "recoil";
 import { v4 as uunidv4 } from "uuid";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+} from "@chakra-ui/react";
 
 import { listsState } from "../src/hooks/listsState";
 
@@ -34,28 +43,70 @@ export default function Create() {
   };
 
   return (
-    <>
-      <h2>Address</h2>
-      <input type="text" value={address} onChange={handleAddAddress} />
-      <h2>Type</h2>
-      <select onChange={handleChangeType}>
-        <option value="flat">Flat</option>
-        <option value="detached">Detached</option>
-        <option value="semi-detached">Semi-detached</option>
-        <option value="Terraced">Terraced</option>
-      </select>
-      <h2>Description</h2>
-      <textarea onChange={(e) => setDescription(e.target.value)} />
+    <Container
+      h="100vh"
+      position="relative"
+      backgroundImage="linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)"
+      maxW="auto"
+    >
+      <Box
+        h={{ base: "auto", md: 600 }}
+        my="6"
+        boxShadow="lg"
+        rounded="lg"
+        w={{
+          base: "calc(100% - 40px)",
+          md: 1000,
+        }}
+        m="auto"
+        p={8}
+        background="#fff"
+        position="absolute"
+        top={{ base: 10, md: "50%" }}
+        left="50%"
+        transform={{
+          base: "translate(-50%, 0)",
+          md: "translate(-50%, -50%)",
+        }}
+      >
+        <Heading as="h1" size="xl" mb={10} color="teal.400">
+          Create a List.
+        </Heading>
+        <Heading as="h2" size="md" mb={2} color="gray.500">
+          Address
+        </Heading>
+        <Input type="text" value={address} mb={8} onChange={handleAddAddress} />
+        <Heading as="h2" size="md" mb={2} color="gray.500">
+          Type
+        </Heading>
+        <Select onChange={handleChangeType} mb={8}>
+          <option value="flat">Flat</option>
+          <option value="detached">Detached</option>
+          <option value="semi-detached">Semi-detached</option>
+          <option value="Terraced">Terraced</option>
+        </Select>
+        <Heading as="h2" size="md" mb={2} color="gray.500">
+          Description
+        </Heading>
+        <Textarea mb={10} onChange={(e) => setDescription(e.target.value)} />
 
-      <Link href="/">
-        <button
-          onClick={() => {
-            handleClickCreate();
-          }}
-        >
-          Create
-        </button>
-      </Link>
-    </>
+        <Link href="/">
+          <Button
+            size="lg"
+            w={200}
+            borderRadius={20}
+            bg={"teal.400"}
+            colorScheme="teal"
+            display="block"
+            mx="auto"
+            onClick={() => {
+              handleClickCreate();
+            }}
+          >
+            Create
+          </Button>
+        </Link>
+      </Box>
+    </Container>
   );
 }
